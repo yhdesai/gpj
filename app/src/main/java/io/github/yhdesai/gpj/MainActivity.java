708e69e0e40337e18bd822b24cfda3f8a5b653ed
Yash <yhdesai99@ymail.com>
2018-01-07 18:50:02 +0530

    commit

--- app/src/main/java/io/github/yhdesai/gpj/MainActivity.java
+++ app/src/main/java/io/github/yhdesai/gpj/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Click on the button at the top left corner", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
